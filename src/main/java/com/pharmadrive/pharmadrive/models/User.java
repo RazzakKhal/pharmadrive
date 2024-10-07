e9a168c4a5a1474fe3fc2cede88a0feaec1c8b71
gayetlyan <90770999+gayetlyan@users.noreply.github.com>
2024-10-07 16:24:16 +0200

    new branch and User.java

---
+++ src/main/java/com/pharmadrive/pharmadrive/models/User.java
@@ -0,0 +1,70 @@
+package com.pharmadrive.pharmadrive.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SourceType;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+
+@Table(name="USERS",indexes = @Index(name = "USERS_index", columnList = "email"))
+@Entity
+@NoArgsConstructor
+@Data
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    @Column(name = "created_at")
+    @CreationTimestamp(source = SourceType.DB)
+    private Instant createdAt;
+    @Column(name = "updated_at")
+    @UpdateTimestamp(source = SourceType.DB)
+    private Instant updatedAt;
+    @OneToMany(mappedBy = "user")
+    private List<Message> messages;
+    @OneToMany(mappedBy = "user")
+    private List<Rental> rentals;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
