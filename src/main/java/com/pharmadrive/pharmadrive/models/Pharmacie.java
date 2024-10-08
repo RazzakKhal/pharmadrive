54fd7590845677031d7687fd2d6333e7dd981d6e
gayetlyan <90770999+gayetlyan@users.noreply.github.com>
2024-10-08 12:24:02 +0200

    final models

--- src/main/java/com/pharmadrive/pharmadrive/models/Pharmacie.java
+++ src/main/java/com/pharmadrive/pharmadrive/models/Pharmacie.java
@@ -1,31 +1,31 @@
-// package com.pharmadrive.pharmadrive.models;
+package com.pharmadrive.pharmadrive.models;
 
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Table;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// @Table(name = "PHARMACIE")
-// @Entity
-// @NoArgsConstructor
-// @Data
-// public class Pharmacie {
+@Table(name = "PHARMACIE")
+@Entity
+@NoArgsConstructor
+@Data
+public class Pharmacie {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long idPharmacie;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPharmacie;
 
-//     @Column(name = "adressPharmacie", nullable = false)
-//     private String adressPharmacie;
+    @Column(name = "adressPharmacie", nullable = false)
+    private String adressPharmacie;
 
-//     @Column(name = "nomPharmacie", nullable = false)
-//     private String nomPharmacie;
+    @Column(name = "nomPharmacie", nullable = false)
+    private String nomPharmacie;
 
-//     @Column(name = "numero", nullable = false)
-//     private String numero;  
+    @Column(name = "numero", nullable = false)
+    private String numero;  
 
-// }
+}
--- src/main/java/com/pharmadrive/pharmadrive/models/PharmacieArticleKey.java
+++ src/main/java/com/pharmadrive/pharmadrive/models/PharmacieArticleKey.java
@@ -1,50 +1,50 @@
-// package com.pharmadrive.pharmadrive.models;
+package com.pharmadrive.pharmadrive.models;
 
-// import java.io.Serializable;
-// import java.util.Objects;
+import java.io.Serializable;
+import java.util.Objects;
 
-// import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embeddable;
 
-// @Embeddable
-// public class PharmacieArticleKey implements Serializable {
+@Embeddable
+public class PharmacieArticleKey implements Serializable {
 
-//     private Long idPharmacie;
-//     private Long idArticle;
+    private Long idPharmacie;
+    private Long idArticle;
 
-//     public PharmacieArticleKey() {}
+    public PharmacieArticleKey() {}
 
-//     public PharmacieArticleKey(Long idPharmacie, Long idArticle) {
-//         this.idPharmacie = idPharmacie;
-//         this.idArticle = idArticle;
-//     }
+    public PharmacieArticleKey(Long idPharmacie, Long idArticle) {
+        this.idPharmacie = idPharmacie;
+        this.idArticle = idArticle;
+    }
 
-//     // Getters et Setters
-//     public Long getIdPharmacie() {
-//         return idPharmacie;
-//     }
+    // Getters et Setters
+    public Long getIdPharmacie() {
+        return idPharmacie;
+    }
 
-//     public void setIdPharmacie(Long idPharmacie) {
-//         this.idPharmacie = idPharmacie;
-//     }
+    public void setIdPharmacie(Long idPharmacie) {
+        this.idPharmacie = idPharmacie;
+    }
 
-//     public Long getIdArticle() {
-//         return idArticle;
-//     }
+    public Long getIdArticle() {
+        return idArticle;
+    }
 
-//     public void setIdArticle(Long idArticle) {
-//         this.idArticle = idArticle;
-//     }
+    public void setIdArticle(Long idArticle) {
+        this.idArticle = idArticle;
+    }
 
-//     @Override
-//     public boolean equals(Object o) {
-//         if (this == o) return true;
-//         if (o == null || getClass() != o.getClass()) return false;
-//         PharmacieArticleKey that = (PharmacieArticleKey) o;
-//         return Objects.equals(idPharmacie, that.idPharmacie) && Objects.equals(idArticle, that.idArticle);
-//     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PharmacieArticleKey that = (PharmacieArticleKey) o;
+        return Objects.equals(idPharmacie, that.idPharmacie) && Objects.equals(idArticle, that.idArticle);
+    }
 
-//     @Override
-//     public int hashCode() {
-//         return Objects.hash(idPharmacie, idArticle);
-//     }
-// }
+    @Override
+    public int hashCode() {
+        return Objects.hash(idPharmacie, idArticle);
+    }
+}
