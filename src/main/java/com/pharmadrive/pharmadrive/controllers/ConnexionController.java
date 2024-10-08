cfeadfc551478735a59ff9eed545367cee47d592
gayetlyan <90770999+gayetlyan@users.noreply.github.com>
2024-10-08 16:35:56 +0200

    controller test

--- src/main/java/com/pharmadrive/pharmadrive/controllers/ConnexionController.java
+++ src/main/java/com/pharmadrive/pharmadrive/controllers/ConnexionController.java
@@ -1,14 +1,26 @@
 package com.pharmadrive.pharmadrive.controllers;
 
+import com.pharmadrive.pharmadrive.models.User;
+import com.pharmadrive.pharmadrive.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class ConnexionController {
 
-    @GetMapping("/hello")
-    public String helloWolrd(){
+    @Autowired
+    UserRepository userRepository;
 
-        return "hello world";
+    @GetMapping("/hello")
+    public User helloWolrd(){
+
+        Optional<User> user = userRepository.findByEmail("test@test.com");
+        if(user.isPresent()){
+            return user.get();
+        }
+        throw new RuntimeException("helloworld");
     }
 }
