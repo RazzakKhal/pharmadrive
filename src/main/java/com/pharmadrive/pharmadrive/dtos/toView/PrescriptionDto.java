bb026cfd52f9db747bd62a2b63d469479a3449b6
gayetlyan <90770999+gayetlyan@users.noreply.github.com>
2024-10-09 15:12:32 +0200

    dto and mapper for prescription

---
+++ src/main/java/com/pharmadrive/pharmadrive/dtos/toView/PrescriptionDto.java
@@ -0,0 +1,30 @@
+package com.pharmadrive.pharmadrive.dtos.toView;
+
+import java.time.Instant;
+
+public class PrescriptionDto {
+
+    private Long id;
+    private String imageOrdonnance;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public PrescriptionDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageOrdonnance() {
+        return imageOrdonnance;
+    }
+
+    public void setImageOrdonnance(String imageOrdonnance) {
+        this.imageOrdonnance = imageOrdonnance;
+    }
+}
