d2c2d26e384659840c3d2dc5412a5b63519e8b85
razzak <khalfallah.razzak@gmail.com>
2024-10-11 02:38:54 +0200

    add of feature for thanks page

--- src/main/java/com/pharmadrive/pharmadrive/controllers/PharmacyController.java
+++ src/main/java/com/pharmadrive/pharmadrive/controllers/PharmacyController.java
@@ -3,10 +3,7 @@ package com.pharmadrive.pharmadrive.controllers;
 import com.pharmadrive.pharmadrive.dtos.toView.PharmacieDto;
 import com.pharmadrive.pharmadrive.services.PharmacyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,10 @@ public class PharmacyController {
     @GetMapping("")
     List<PharmacieDto> getAllPharmacies(){
     return pharmacyService.getAllPharmacies();
+    }
+
+    @GetMapping("/{id}")
+    PharmacieDto getPharmacy(@PathVariable Long id){
+        return pharmacyService.getPharmacy(id);
     }
 }
--- src/main/java/com/pharmadrive/pharmadrive/services/PharmacyService.java
+++ src/main/java/com/pharmadrive/pharmadrive/services/PharmacyService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface PharmacyService {
 
     List<PharmacieDto> getAllPharmacies();
+
+    PharmacieDto getPharmacy(Long id);
 }
